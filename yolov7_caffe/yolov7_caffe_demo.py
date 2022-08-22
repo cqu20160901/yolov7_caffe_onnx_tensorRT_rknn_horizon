c68cc4cf767945279eeee700a01f529fb63ff783
业余程序猿 <1013429609@qq.com>
2022-08-22 13:22:26 +0800

    Update yolov7_caffe_demo.py

--- yolov7_caffe/yolov7_caffe_demo.py
+++ yolov7_caffe/yolov7_caffe_demo.py
@@ -164,10 +164,41 @@ def postprocess(out, img_h, img_w):
     return predBox
 
 
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
 def preprocess(src):
-    img = cv2.resize(src, (input_imgW, input_imgH))
+    #img = cv2.resize(src, (input_imgW, input_imgH), interpolation=cv2.INTER_LINEAR)
+    img, ratio, (dw, dh) = letterbox(src, new_shape=(input_imgW, input_imgW))
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     img = img.astype(np.float32)
-    img = img * 0.00392156
+    img = img / 255
     return img
 
 
